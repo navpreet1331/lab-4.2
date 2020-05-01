b31e924d83f32b60576171436d7e1dcd3e6f2f2b
navpreet1331 <45588234+navpreet1331@users.noreply.github.com>
2020-05-01 08:50:22 +0530

    Add files via upload

---
+++ lab-4.2-versioning-master/src/main/java/navpreet/cg/gla/spring/dao/ProductDaoInterface.java
@@ -0,0 +1,15 @@
+package navpreet.cg.gla.spring.dao;
+
+import java.util.List;
+
+import arun.cg.gla.spring.entity.Product;
+import arun.cg.gla.spring.entity.ProductOld;
+
+
+public interface ProductDaoInterface {
+	
+	public boolean create(Product product);
+	public List<Product> reterivev1();
+//	public List<ProductOld> reterieve();
+
+}
